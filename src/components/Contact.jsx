672d322f86d832a71a72988d319c3ce9b2c300e4
Hindol19 <hindolbanerjee5@gmail.com>
2024-07-05 01:51:07 +0530

    added email portion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import EmailIcon from "@mui/icons-material/Email";
+import Link from "next/link";
 const Contact = () => {
   return (
     <div className="w-full flex flex-col">
@@ -8,8 +9,14 @@ const Contact = () => {
         <div className="mt-3 w-[120px] border-b-2 border-light"></div>
       </div>
       <div className="flex flex-row items-center justify-center my-[80px]">
-        <div className="w-[500px] h-[400px] border-2 border-light mr-[-100px]">
-          Contact
+        <div className="w-[500px] h-[400px] bg-gradient-to-r from-violet-600 to-[#4f46e55e] bg-opacity-[20%] mr-[-100px] flex flex-col justify-center items-center z-50 backdrop-blur-sm">
+          <div className="text-2xl">Email me here 😁</div>
+          <Link
+            href="mailto:musicmakerr55@gmail.com"
+            className="w-[200px] bg-alt1 py-3 my-5 flex justify-center items-center rounded-md "
+          >
+            <EmailIcon className="text-dark" />
+          </Link>
         </div>
         <div className="w-[800px] h-[500px] border-2 border-alt1"></div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="flex flex-row top-4 left-[50%] translate-x-[-50%] fixed bg-light bg-opacity-50 rounded-lg px-5 py-3 text-dark font-medium backdrop-blur-md">
+      <div className="flex flex-row top-4 left-[50%] translate-x-[-50%] fixed bg-light bg-opacity-50 rounded-lg px-5 py-3 text-dark font-medium backdrop-blur-md z-50">
         <div className="px-3 cursor-pointer">About</div>
         <div className="px-3 cursor-pointer">Contact</div>
         <div className="px-3 cursor-pointer">Other Services</div>
